ece442c130dee03aafc69d21c34d63833e55064c
Kartik <aggarwalkartik96@gmail.com>
2020-06-21 00:56:39 +0530

    Updates

---
+++ precache-manifest.dd8842292aefe525e15616b75937a063.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69255074c09a1ea74fa4360880ab9bd8",
+    "url": "/spacexapp/index.html"
+  },
+  {
+    "revision": "50ae50a3e82079eb3abf",
+    "url": "/spacexapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0fa4fafdbdf936437573",
+    "url": "/spacexapp/static/css/main.1777866d.chunk.css"
+  },
+  {
+    "revision": "50ae50a3e82079eb3abf",
+    "url": "/spacexapp/static/js/2.0b1d3b88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spacexapp/static/js/2.0b1d3b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa4fafdbdf936437573",
+    "url": "/spacexapp/static/js/main.20acfca5.chunk.js"
+  },
+  {
+    "revision": "5390f412e2efd14847d3",
+    "url": "/spacexapp/static/js/runtime-main.4198ffec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spacexapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6cd2fc7edccca9bed588bed1f3e63a2c",
+    "url": "/spacexapp/static/media/logout_img.6cd2fc7e.png"
+  }
+]);
