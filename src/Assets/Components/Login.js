de4fe39ea6d0b969c0b25922d1e42c4cf524d76e
Kartik <aggarwalkartik96@gmail.com>
2020-10-22 14:28:28 +0530

    Style changes

--- src/Assets/Components/Login.js
+++ src/Assets/Components/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import axios from "axios"
 import "../Styles/login.css"
-import {Redirect} from "react-router-dom"
 
 const loginurl = "https://run.mocky.io/v3/689bee42-50f3-4da7-9046-64bde3a0317b"
 
